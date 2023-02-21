6316514f8bf50019cf1057203702ef5e7403bb78
Gergely <gergely.huszar94@gmail.com>
2023-02-21 14:55:45 +0100

    add CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import RandomCats from "./pages/RandomCats";
 import CatBreeds from "./pages/CatBreeds";
 import FavouriteCats from "./pages/FavouriteCats";
+import { CssBaseline } from "@mui/material";
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RandomCats />} />
-        <Route path="/cat-breeds" element={<CatBreeds />} />
-        <Route path="/favourite-cats" element={<FavouriteCats />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RandomCats />} />
+          <Route path="/cat-breeds" element={<CatBreeds />} />
+          <Route path="/favourite-cats" element={<FavouriteCats />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
