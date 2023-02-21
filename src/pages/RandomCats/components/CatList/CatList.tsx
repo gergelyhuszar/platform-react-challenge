dd086e81f4c1b5bf66645f83d6ab86fdc2a8edd5
Gergely <gergely.huszar94@gmail.com>
2023-02-21 14:58:15 +0100

    use CatModal in CatList

--- src/pages/RandomCats/components/CatList/CatList.tsx
+++ src/pages/RandomCats/components/CatList/CatList.tsx
@@ -3,36 +3,44 @@ import { Grid } from "@mui/material";
 import CatImage from "./CatImage";
 import { getCats } from "apis/catApi";
 import { Cat } from "types/cat";
+import CatModal from "./CatModal";
 
 const CatList: FC = () => {
-  const [catList, setCatList] = useState<Cat[]>();
+  const [catList, setCatList] = useState<Cat[]>([]);
+  const [selectedCat, setSelectedCat] = useState<Cat | null>(null);
 
   useEffect(() => {
     (async () => {
       const cats = await getCats();
-      console.log(cats)
       setCatList(cats);
     })();
   }, []);
 
   return (
-    <Grid
-      item
-      container
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      width="80%"
-      marginBottom="50px"
-    >
+    <>
+      <Grid
+        item
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        width="80%"
+        marginBottom="50px"
+      >
+        {
+          catList.map((cat) => (
+            <Grid item key={cat.id} marginTop="50px" marginLeft="50px">
+              <CatImage cat={cat} onClick={() => setSelectedCat(cat)} />
+            </Grid>
+          ))
+        }
+      </Grid>
       {
-        catList?.map((cat) => (
-          <Grid item key={cat.id} marginTop="50px" marginLeft="50px">
-            <CatImage cat={cat} />
-          </Grid>
-        ))
+        selectedCat && (
+          <CatModal open={!!selectedCat} onClose={() => setSelectedCat(null)} cat={selectedCat} />
+        )
       }
-    </Grid>
+    </>
   );
 };
 
