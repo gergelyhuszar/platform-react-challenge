2c5710448b22fe6aa8f55e59c198cd409b77b437
Gergely <gergely.huszar94@gmail.com>
2023-02-21 12:37:01 +0100

    add getCats api

--- src/apis/catApi.ts
+++ src/apis/catApi.ts
@@ -1,1 +1,25 @@
-export {};
+import axios from "axios";
+import { Cat } from "types/cat";
+
+const api = axios.create({
+  baseURL: "https://api.thecatapi.com/v1",
+  headers: {
+    "x-api-key": "live_bmkDVOnAzL5lyaqfJyObwATnKfBPyA3u2k1clOeTENerKpVJPPV6hUQKEvwhDB9l"
+  }
+});
+
+const getCats = async (): Promise<Cat[]> => {
+  let cats: Cat[];
+
+  try {
+    const { data } = await api.get("/images/search?limit=10");
+    cats = data;
+  } catch (error) {
+    cats = [];
+    console.error(error);
+  }
+
+  return cats;
+};
+
+export { getCats };
