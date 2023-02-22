3ba3bd1eb402af45b7fabc807964d317c008a957
Gergely <gergely.huszar94@gmail.com>
2023-02-22 09:37:02 +0100

    use CatContext in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React, { FC } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import RandomCats from "./pages/RandomCats";
-import CatBreeds from "./pages/CatBreeds";
-import FavouriteCats from "./pages/FavouriteCats";
+import RandomCats from "pages/RandomCats";
+import CatBreeds from "pages/CatBreeds";
+import FavouriteCats from "pages/FavouriteCats";
 import Bootstrapper from "components/Bootstrapper/Bootstrapper";
+import CatContextProvider from "context/CatContext";
 
 const App: FC = () => {
   return (
     <Bootstrapper>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<RandomCats />} />
-          <Route path="/cat-breeds" element={<CatBreeds />} />
-          <Route path="/favourite-cats" element={<FavouriteCats />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
+      <CatContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<RandomCats />} />
+            <Route path="/cat-breeds" element={<CatBreeds />} />
+            <Route path="/favourite-cats" element={<FavouriteCats />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </CatContextProvider>
     </Bootstrapper>
   );
 }
